b1ba6155acd007d6923bc8d5a06a9a9b122498a7
adams <108087334+sudyk1@users.noreply.github.com>
2024-07-04 13:52:37 +0200

    04.07.2024 changed importing publications and users

--- src/pl/sudyk/library/io/file/CsvFileManager.java
+++ src/pl/sudyk/library/io/file/CsvFileManager.java
@@ -24,14 +24,14 @@ public class CsvFileManager implements FileManager {
     }
 
     private void importUsers(Library library) {
-        try (Scanner fileReader = new Scanner(new File(USERS_FILE_NAME))) {
-            while (fileReader.hasNextLine()) {
-                String line = fileReader.nextLine();
-                LibraryUser user = createUserFromString(line);
-                library.addUser(user);
-            }
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(USERS_FILE_NAME))) {
+            bufferedReader.lines()
+                    .map(this::createUserFromString)
+                    .forEach(library::addUser);
         } catch (FileNotFoundException e) {
-            throw new DataImportException("Not implemented");
+            throw new DataImportException("File does not exist " + USERS_FILE_NAME);
+        } catch (IOException e) {
+            throw new DataImportException("Failed to read " + USERS_FILE_NAME);
         }
     }
 
@@ -44,15 +44,15 @@ public class CsvFileManager implements FileManager {
     }
 
     private void importPublications(Library library) {
-        try (Scanner fileReader = new Scanner(new File(FILE_NAME))) {
-            while (fileReader.hasNextLine()) {
-                String line = fileReader.nextLine();
-                Publication publication = createObjectFromString(line);
-                library.addPublication(publication);
-            }
-
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE_NAME))) {
+            bufferedReader
+                    .lines()
+                    .map(this::createObjectFromString)
+                    .forEach(library::addPublication);
         } catch (FileNotFoundException e) {
-            throw new DataImportException("Not implemented");
+            throw new DataImportException("File does not exist " + FILE_NAME);
+        } catch (IOException e) {
+            throw new DataImportException("Failed to read " + FILE_NAME);
         }
     }
 
