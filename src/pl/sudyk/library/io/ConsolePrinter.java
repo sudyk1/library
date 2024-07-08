7bf5497e6af622370422fbc6b952d7d8976bf206
adams <108087334+sudyk1@users.noreply.github.com>
2024-07-08 10:57:56 +0200

    08.07.2024 changed printing methods to use streams

--- src/pl/sudyk/library/io/ConsolePrinter.java
+++ src/pl/sudyk/library/io/ConsolePrinter.java
@@ -6,27 +6,23 @@ import java.util.Collection;
 
 public class ConsolePrinter {
     public void printBooks(Collection<Publication> publications) {
-        int countBooks = 0;
-        for (Publication publication : publications) {
-            if (publication instanceof Book) {
-                printLine(publication.toString());;
-                countBooks++;
-            }
-        }
-        if (countBooks == 0) {
+        long count = publications.stream()
+                .filter(pub -> pub instanceof Book)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if (count == 0) {
             printLine("No books in library.");
         }
     }
 
     public void printMagazines(Collection<Publication> publications) {
-        int countMagazines = 0;
-        for (Publication publication : publications) {
-            if (publication instanceof Magazine) {
-                printLine(publication.toString());;
-                countMagazines++;
-            }
-        }
-        if (countMagazines == 0) {
+        long count = publications.stream()
+                .filter(pub -> pub instanceof Magazine)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if (count == 0) {
             printLine("No magazines in library.");
         }
     }
